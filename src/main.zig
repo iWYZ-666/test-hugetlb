0b1f037d5e552e22d7e7b795c595d757d50e3370
Huanlun Cheng <huache01@a015171.shanghai.arm.com>
2025-08-13 13:11:21 +0800

    remove comment code

--- src/main.zig
+++ src/main.zig
@@ -17,8 +17,6 @@ fn test_madvise() !void {
         .TYPE = .PRIVATE,
         .ANONYMOUS = true,
     };
-    // const flag_num: u32 = @bitCast(map_flags);
-    // std.debug.print("map_flags: {d}\n", .{flag_num});
     const mapped_mem: []align(page_size_min) u8 = posix.mmap(null, LARGE, posix.PROT.READ | posix.PROT.WRITE, map_flags, -1, 0) catch |err| {
         std.debug.print("Failed to mmap: {}\n", .{err});
         return err;
@@ -28,14 +26,6 @@ fn test_madvise() !void {
         std.debug.print("Failed to madvise: {}\n", .{err});
     };
     try do_iter(mapped_mem);
-    // const u64_map = std.mem.bytesAsSlice(u64, mapped_mem);
-    // var rand_bytes: [8]u8 = undefined;
-    // const slice = @as([]u8, rand_bytes[0..]);
-    // try posix.getrandom(slice);
-    // const rand_num = std.mem.readInt(u64, @ptrCast(slice), .little);
-    // for (0..ITER) |_| {
-    //     @memset(u64_map, rand_num);
-    // }
 }
 
 fn test_mmap_huge() !void {
@@ -44,22 +34,12 @@ fn test_mmap_huge() !void {
         .ANONYMOUS = true,
         .HUGETLB = true,
     };
-    // const flag_num: u32 = @bitCast(map_flags);
-    // std.debug.print("map_flags: {d}\n", .{flag_num});
     const mapped_mem: []align(page_size_min) u8 = posix.mmap(null, LARGE, posix.PROT.READ | posix.PROT.WRITE, map_flags, -1, 0) catch |err| {
         std.debug.print("Failed to mmap: {}\n", .{err});
         return err;
     };
     defer posix.munmap(mapped_mem);
     try do_iter(mapped_mem);
-    // const u64_map = std.mem.bytesAsSlice(u64, mapped_mem);
-    // var rand_bytes: [8]u8 = undefined;
-    // const slice = @as([]u8, rand_bytes[0..]);
-    // try posix.getrandom(slice);
-    // const rand_num = std.mem.readInt(u64, @ptrCast(slice), .little);
-    // for (0..ITER) |_| {
-    //     @memset(u64_map, rand_num);
-    // }
 }
 
 fn test_normal() !void {
@@ -73,14 +53,6 @@ fn test_normal() !void {
     };
     defer posix.munmap(mapped_mem);
     try do_iter(mapped_mem);
-    // const u64_map = std.mem.bytesAsSlice(u64, mapped_mem);
-    // var rand_bytes: [8]u8 = undefined;
-    // const slice = @as([]u8, rand_bytes[0..]);
-    // try posix.getrandom(slice);
-    // const rand_num = std.mem.readInt(u64, @ptrCast(slice), .little);
-    // for (0..ITER) |_| {
-    //     @memset(u64_map, rand_num);
-    // }
 }
 
 fn do_iter(mapped_mem :[]u8) !void {
